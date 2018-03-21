4e15b44780ac36f240d889f697c7a6d3a3668b9b
Muhammad Saqib <mohammadsaqib1997@gmail.com>
2018-03-21 19:43:50 +0500

    listing search task complete

--- src/partials/components/html_utils/tabel_comp.vue
+++ src/partials/components/html_utils/tabel_comp.vue
@@ -1,30 +1,39 @@
 <template lang="pug">
     div
-        .row
-            .col-md-3.col-md-offset-9
-                .form-group
-                    input.form-control(type='text' placeholder='Search' v-model='search_table')
-        table.table.table-hover.table-click(ref='table')
-            thead
-                slot(name="thead")
-            tbody
-                slot(name="tbody")
-            slot(name="tfoot")
-        .row
-            .col-sm-12.text-center.mt-15
-                label.mr-10 Page: {{curPage}}/{{ gen_pages }}
-                .btn-group
-                    button.btn.btn-sm(v-if='curPage>1' v-on:click='prevPage')
-                        i.fa.fa-chevron-left
-                    button.btn.btn-sm(v-else disabled='disabled')
-                        i.fa.fa-chevron-left
-                    button.btn.btn-sm(v-if='curPage<gen_pages' v-on:click='nextPage')
-                        i.fa.fa-chevron-right
-                    button.btn.btn-sm(v-else disabled='disabled')
-                        i.fa.fa-chevron-right
+      .row
+        .col-md-3.col-md-offset-9
+            .form-group
+              input.form-control(type='text' placeholder='Search' v-model='search_table')
+            .form-group
+              label(style="margin-right:15px;")
+                input.cusCheckBox(type='radio' v-model="r_radio_btn" value="or")
+                | &nbsp;&nbsp;OR
+              label
+                input.cusCheckBox(type='radio' v-model="r_radio_btn" value="and")
+                | &nbsp;&nbsp;AND
+                  
+      table.table.table-hover.table-click(ref='table')
+          thead
+              slot(name="thead")
+          tbody
+              slot(name="tbody")
+          slot(name="tfoot")
+      .row
+          .col-sm-12.text-center.mt-15
+              label.mr-10 Page: {{curPage}}/{{ gen_pages }}
+              .btn-group
+                  button.btn.btn-sm(v-if='curPage>1' v-on:click='prevPage')
+                      i.fa.fa-chevron-left
+                  button.btn.btn-sm(v-else disabled='disabled')
+                      i.fa.fa-chevron-left
+                  button.btn.btn-sm(v-if='curPage<gen_pages' v-on:click='nextPage')
+                      i.fa.fa-chevron-right
+                  button.btn.btn-sm(v-else disabled='disabled')
+                      i.fa.fa-chevron-right
 </template>
 
 <script>
+import _ from "lodash";
 export default {
   name: "tabel_comp",
   props: {
@@ -48,6 +57,12 @@ export default {
     });
   },
   watch: {
+    r_radio_btn (val) {
+      this.s_restrict = (val === "or") ? false:true;
+    },
+    s_restrict (val) {
+      this.search_row(this.search_table);
+    },
     search_table(val) {
       this.search_row(val);
     },
@@ -59,11 +74,13 @@ export default {
   data() {
     return {
       search_table: "",
+      r_radio_btn: "and",
       table: null,
       gen_pages: 0,
       curPage: 1,
       visible_rows: null,
       start: 0,
+      s_restrict: true,
       indexes: {
         status: {
           activate: 1,
@@ -78,19 +95,25 @@ export default {
       const $table = $(self.table);
       const $rows = $table.find("tbody tr");
 
-      let searches = val.split(";");
+      let searches = _.filter(val.split(";"), function(srh) {
+        if (srh !== "") {
+          return srh;
+        }
+      });
 
-      if (searches[0] !== "") {
+      if (searches.length > 0 && searches[0] !== "") {
+        let v_rows = {};
         $rows.addClass("hidden");
-        $rows.each(function() {
+        $rows.each(function(row_ind) {
           const $row = $(this);
           const columns = $row.find("td");
-          let v_row = false;
+          let validate_col = {};
 
-          columns.each(function() {
+          columns.each(function(col_ind) {
+            validate_col[col_ind] = {};
             const $col = $(this);
             const attr_get = $col.attr("data-indx");
-            searches.forEach(function(srh) {
+            searches.forEach(function(srh, s_ind) {
               if (srh !== "") {
                 let split_sval = srh.split(":");
                 if (
@@ -99,29 +122,50 @@ export default {
                 ) {
                   const col_ind_val = $col.attr("data-search");
                   if (
+                    self.indexes.hasOwnProperty(split_sval[0]) &&
                     self.indexes[split_sval[0]].hasOwnProperty([split_sval[1]])
                   ) {
                     if (
                       self.indexes[split_sval[0]][split_sval[1]] == col_ind_val
                     ) {
-                      v_row = self.multiCheck(searches, v_row);
+                      validate_col[col_ind][s_ind] = true;
                     }
+                  } else if (split_sval[1] === col_ind_val) {
+                    validate_col[col_ind][s_ind] = true;
                   }
                 } else {
                   let txt = $col.text().toLowerCase();
-                  let val = split_sval[0].toLowerCase();
-                  if (txt.indexOf(val) > -1) {
-                    v_row = self.multiCheck(searches, v_row);
+                  let s_val = split_sval[0].toLowerCase();
+                  if (txt.indexOf(s_val) > -1) {
+                    validate_col[col_ind][s_ind] = true;
                   }
                 }
               }
             });
           });
 
-          if (v_row) {
-            $row.removeClass("hidden");
-          }
+          v_rows[row_ind] = self.multiCheck(searches, validate_col);
         });
+
+        row_loop: for (let [key, val] of Object.entries(v_rows)) {
+          let find_s_row = {};
+          columns: for (let [c_key, c_val] of Object.entries(val.valid_col)) {
+            if (self.s_restrict) {
+              for (let s_ind in c_val) {
+                find_s_row[s_ind] = true;
+                if (val.srh_length === Object.keys(find_s_row).length) {
+                  $rows.eq(key).removeClass("hidden");
+                  continue row_loop;
+                }
+              }
+            } else {
+              if (!_.isEmpty(c_val)) {
+                $rows.eq(key).removeClass("hidden");
+                break columns;
+              }
+            }
+          }
+        }
       } else {
         $rows.removeClass("hidden");
       }
@@ -162,17 +206,8 @@ export default {
     nextPage() {
       this.curPage += 1;
     },
-    multiCheck(searches, prev_col_val) {
-      if (searches.length > 1) {
-          console.log(searches, prev_col_val);
-        if (prev_col_val) {
-          return true;
-        } else {
-          return true;
-        }
-      } else {
-        return true;
-      }
+    multiCheck(searches, validate_col) {
+      return { srh_length: searches.length, valid_col: validate_col };
     }
   }
 };
