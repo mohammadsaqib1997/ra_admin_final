c52eb0a7893f4b0726d9941ac476fee1ab51eef2
Muhammad Saqib <mohammadsaqib1997@gmail.com>
2018-03-28 14:09:04 +0500

    Website changes fix 1

--- public/javascripts/js/custom.js
+++ public/javascripts/js/custom.js
@@ -190,7 +190,7 @@ $(document).ready(function () {
     var owl = $("#screenshots");
 
     owl.owlCarousel({
-        items: 4, //10 items above 1000px browser width
+        items: 3, //10 items above 1000px browser width
         itemsDesktop: [1000, 4], //5 items between 1000px and 901px
         itemsDesktopSmall: [900, 2], // betweem 900px and 601px
         itemsTablet: [600, 1], //2 items between 600 and 0
